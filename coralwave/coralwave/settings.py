7a024ffe32973d1ac8785b862baf58041b9f416b
Merrcurys <a.sebezhko2017@gmail.com>
2025-06-21 02:13:36 +0300

    five

--- coralwave/coralwave/settings.py
+++ coralwave/coralwave/settings.py
@@ -25,7 +25,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'store'
+    'rest_framework',
+    'store',
+    'api_shop'
 ]
 
 MIDDLEWARE = [
@@ -113,3 +115,12 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10,
+    'DEFAULT_RENDERER_CLASSES': [
+        'rest_framework.renderers.JSONRenderer',
+        'rest_framework.renderers.BrowsableAPIRenderer',
+    ]
+}
--- coralwave/coralwave/urls.py
+++ coralwave/coralwave/urls.py
@@ -3,5 +3,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include("store.urls"))
+    path('', include("store.urls")),
+    path('api/', include('api_shop.urls')),
+    path('api-auth/', include('rest_framework.urls'))
 ]
