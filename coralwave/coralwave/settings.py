c7a25cab99917683a9a7669bbba345ed8c2fe387
Merrcurys <a.sebezhko2017@gmail.com>
2025-06-21 03:21:18 +0300

    add host

--- coralwave/coralwave/settings.py
+++ coralwave/coralwave/settings.py
@@ -11,9 +11,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-3nkd=3!o$h%b8#qwj54ctax9i%lo@s!(hxr3r10o3+b2vmqphy'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['merrcurys.pythonanywhere.com']
 
 
 # Application definition
